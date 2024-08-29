e8defcefd2d1f8d6dc37a4df6c315ae43ae151c3
PriangSu <tushersaha467@gmail.com>
2024-08-29 21:45:32 +0600

    Simple Login UI in Android (Kotlin)

---
+++ app/src/main/java/com/example/logingclone/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.logingclone
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.logingclone.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.loginB.setOnClickListener {
+            val userName =binding.userName.text.toString()
+
+            Toast.makeText(this@MainActivity, userName, Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
