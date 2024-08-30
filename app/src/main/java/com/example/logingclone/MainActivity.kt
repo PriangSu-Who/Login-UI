0e2916367398dce4e78e6127be1ee76a5c43625e
PriangSu <tushersaha467@gmail.com>
2024-08-31 02:37:35 +0600

    Simple Login UI in Android (Kotlin)
    Added a Show password and hide password button

--- app/src/main/java/com/example/logingclone/MainActivity.kt
+++ app/src/main/java/com/example/logingclone/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.logingclone
-
 import android.os.Bundle
+import android.text.InputType
+import android.widget.CompoundButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.logingclone.databinding.ActivityMainBinding
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),CompoundButton.OnCheckedChangeListener {
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +17,21 @@ class MainActivity : AppCompatActivity() {
         
         binding.loginB.setOnClickListener {
             val userName =binding.userName.text.toString()
-
             Toast.makeText(this@MainActivity, userName, Toast.LENGTH_SHORT).show()
-
         }
+        binding.switchB.setOnCheckedChangeListener(this)
+    }
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+
+        if (isChecked == true){
+            binding.password.inputType =InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            Toast.makeText(this@MainActivity, "Password Visible", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            binding.password.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            Toast.makeText(this@MainActivity, "Switch is off", Toast.LENGTH_SHORT).show()
+        }
+        binding.password.setSelection(binding.password.text.length)
+
     }
 }
